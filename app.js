70c8a5101c800e6ed195f37d54d7be6ba038be74
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-09-29 23:11:53 +0100

    initial setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port ${3000}...`));
